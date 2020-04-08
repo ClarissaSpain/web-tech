5df49c214f636dd3708cab955339d13ad17ce62b
Clarissa Spain <35749695+ClarissaSpain@users.noreply.github.com>
2020-04-08 13:03:33 -0600

    update

---
+++ HW-11/myScript.js
@@ -0,0 +1,9 @@
+//work in progesss
+var ctx = document.getElementById('game').getContext('2d');
+// load image
+var image = new Image();
+image.onload = function () {
+    // draw the image into the canvas
+    ctx.drawImage(image, 0, 0);
+}
+image.src = './assets/monster_sprite.png';
